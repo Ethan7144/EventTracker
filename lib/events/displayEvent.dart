cce8db54832cf30abff49995aef9426dda86e5d3
Ethan7144 <ejtarlov@me.com>
2023-04-20 17:32:30 -0700

    Added toggle to show only upcoming events

--- lib/events/displayEvent.dart
+++ lib/events/displayEvent.dart
@@ -11,6 +11,7 @@ class MyEventsPage extends StatefulWidget {
 
 class _MyEventsPageState extends State<MyEventsPage> {
   List<Event> events = [];
+  bool showOnlyUpcoming = false; // add a boolean flag to toggle between showing all events and only upcoming events
 
   void _showNewEventForm() {
     showDialog(
@@ -52,29 +53,29 @@ class _MyEventsPageState extends State<MyEventsPage> {
   }
 
   Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final upcomingEvents = events.where((event) => event.endDate.isAfter(now)).toList();
+
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             SizedBox(height: 16),
-            ElevatedButton(
-              child: Text('Add Event'),
-              onPressed: _showNewEventForm,
-            ),
-            SizedBox(height: 16),
             Expanded(
               child: ListView.builder(
-                itemCount: events.length,
+                itemCount: showOnlyUpcoming ? upcomingEvents.length : events.length,
                 itemBuilder: (BuildContext context, int index) {
+                  final event = showOnlyUpcoming ? upcomingEvents[index] : events[index];
+
                   return InkWell(
                     onTap: () {
-                      _showEditDateForm(events[index]);
+                      _showEditDateForm(event);
                     },
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
                         Text(
-                          events[index].title,
+                          event.title,
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 16,
@@ -82,7 +83,7 @@ class _MyEventsPageState extends State<MyEventsPage> {
                         ),
                         const SizedBox(height: 4),
                         Text(
-                          '${DateFormat.yMMMd().add_jm().format(events[index].startDate)} - ${DateFormat.yMMMd().add_jm().format(events[index].endDate)}',
+                          '${DateFormat.yMMMd().add_jm().format(event.startDate)} - ${DateFormat.yMMMd().add_jm().format(event.endDate)}',
                           style: const TextStyle(
                             color: Colors.grey,
                             fontSize: 12,
@@ -90,7 +91,7 @@ class _MyEventsPageState extends State<MyEventsPage> {
                         ),
                         const SizedBox(height: 4),
                         Text(
-                          events[index].description,
+                          event.description,
                           style: const TextStyle(
                             color: Colors.grey,
                             fontSize: 12,
@@ -99,7 +100,7 @@ class _MyEventsPageState extends State<MyEventsPage> {
                         ElevatedButton(
                           onPressed: () {
                             setState(() {
-                              events.removeAt(index);
+                              events.remove(event);
                             });
                           },
                           child: Text('Delete'),
@@ -110,6 +111,26 @@ class _MyEventsPageState extends State<MyEventsPage> {
                   );
                 },
               ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              child: Text('Add Event'),
+              onPressed: _showNewEventForm,
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Checkbox(
+                  value: showOnlyUpcoming,
+                  onChanged: (value) {
+                    setState(() {
+                      showOnlyUpcoming = value!;
+                    });
+                  },
+                ),
+                Text('Show only upcoming events'),
+              ],
             ),
           ],
         ),
