7ef4d177c52a78c34269577aed439f9dc09bf0a0
Ethan7144 <ejtarlov@me.com>
2023-05-04 20:05:04 -0700

    Made the home stay still

--- lib/events/event_details.dart
+++ lib/events/event_details.dart
@@ -22,62 +22,67 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
     final int length = eventsModel.eventsListSize;
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      event.title,
-                      style: Theme.of(context).textTheme.headlineLarge,
-                    ),
-                    Text(
-                      event.description,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    Text(
-                      'Start Date: ${DateFormat.yMd().format(event.startDate)}',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    Text(
-                      'End Date: ${DateFormat.yMd().format(event.endDate)}',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                  ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                    icon: const Icon(Icons.arrow_back_ios),
+                    onPressed: () => GoRouter.of(context).pop(),
+                  ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        event.title,
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
+                      Text(
+                        event.description,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      Text(
+                        'Start Date: ${DateFormat.yMd().format(event.startDate)}',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      Text(
+                        'End Date: ${DateFormat.yMd().format(event.endDate)}',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                if (widget.index > 0)
-                  ElevatedButton(
-                    child: const Text('Previous'),
-                    onPressed: () => context.push('/event/${widget.index - 1}'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  widget.index > 0 ?
+                    ElevatedButton(
+                      child: const Text('Previous'),
+                      onPressed: () => context.push('/event/${widget.index - 1}'),
+                    ) : const TextButton(onPressed: null, child: Text("")),
+                    ElevatedButton(
+                    onPressed: () => context.push('/home'),
+                    child: const Text('Home'),
                   ),
-                if (widget.index < length - 1)
-                  ElevatedButton(
-                    child: const Text('Next'),
-                    onPressed: () => context.push('/event/${widget.index + 1}'),
-                  ),
-                IconButton(
-                  icon: const Icon(Icons.arrow_back_ios),
-                  onPressed: () => GoRouter.of(context).pop(),
-                ),
-                ElevatedButton(
-                  onPressed: () => context.push('/home'),
-                  child: const Text('Home'),
-                ),
-              ],
-            ),
-          ],
+                  widget.index < length - 1 ? 
+                    ElevatedButton(
+                      child: const Text('Next'),
+                      onPressed: () => context.push('/event/${widget.index + 1}'),
+                    ) : const TextButton(onPressed: null, child: Text("")),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
